98b180317751eb5114118de474ed70451febea54
Jack Dewinter <Jack.Dewinter@red-gate.com>
2019-05-03 23:00:28 +0100

    Add experience section

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,15 +1,44 @@
 <template>
-	<b-container class="Education">
+  <b-container class="Experience">
+    <h2>Experience</h2>
 
-	</b-container>
+    <ul>
+      <li class="Experience__item">
+        <div class="Experience__item__row">
+          <h3>Willy Wonka's Factory</h3>
+          <p>March 2017 (3 months)</p>
+        </div>
+        <p>Intern as chocolate stirrer</p>
+        <div class="Experience__item__row">
+          <h4>Projects</h4>
+        </div>
+        <ul>
+          <li>Built a cool app</li>
+        </ul>
+      </li>
+    </ul>
+  </b-container>
 </template>
 
 <style lang="scss">
-@import '../styles/index.scss';
+@import "../styles/index.scss";
 
-.Education {
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
+.Experience {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  &__item {
+    display: flex;
+    flex-direction: column;
+    align-content: flex-start;
+    text-align: left;
+
+    &__row {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+  }
 }
 </style>
