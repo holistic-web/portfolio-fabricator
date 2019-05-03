9e2e17af13fc6d8997740c2940ba5ce172da19ef
michaelfitzhavey <michael.fitzhavey@gmail.com>
2019-05-03 22:30:41 +0100

    feat: education section

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,6 +1,25 @@
 <template>
 	<b-container class="Education">
 
+		<h2>Education</h2>
+
+		<ul>
+			<li class="Education__item">
+				<div class="Education__item__row">
+					<h3>Night's Watch</h3>
+					<span>Seasons 2-5ish</span>
+				</div>
+				<p>I helped protect the world from the cold for a lil bit.</p>
+				<h4>Projects</h4>
+				<ul>
+					<li>
+						I died once, it taught me a lot.
+					</li>
+				</ul>
+			</li>
+
+		</ul>
+
 	</b-container>
 </template>
 
@@ -11,5 +30,19 @@
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
+
+	&__item {
+		display: flex;
+		flex-direction: column;
+		align-content: flex-start;
+		text-align: left;
+
+		&__row {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+		}
+
+	}
 }
 </style>
