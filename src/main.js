41a00364ad2cafe4189017e1e65f72b2fa4769e1
michaelfitzhavey <michael.fitzhavey@gmail.com>
2019-05-04 16:08:03 +0100

    feat: add store

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import App from './App.vue';
+import store from './store';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
 new Vue({
+	store,
 	render: h => h(App)
 }).$mount('#app');
---
+++ src/store.js
@@ -0,0 +1,14 @@
+// Make sure to call Vue.use(Vuex) first if using a module system
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+	state: {
+		context: {
+
+		}
+	}
+});
