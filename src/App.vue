5818ec7787e6c14835b03b69f0143470f7c8769a
Charlie Lancaster <charlie.lanc@gmail.com>
2019-05-06 16:37:38 +0100

    feat: changed main font

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ export default {
 @import './styles/helper-classes.scss';
 @import './styles/defaults/a.scss';
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	font-family: 'Montserrat', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	width: 100%;
