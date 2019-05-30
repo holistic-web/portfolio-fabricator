9f01a3ccc01e55dbef35396ea3ea665b496f715d
Charlie Lancaster <charlie.lanc@gmail.com>
2019-05-30 23:51:13 +0100

    feat: added login component to index

--- portfolio-management/admin-ui/src/containers/authentication/index.js
+++ portfolio-management/admin-ui/src/containers/authentication/index.js
@@ -1,10 +1,16 @@
 import SignUp from './SignUp.vue';
+import LogIn from './LogIn.vue';
 
 const routes = [
 	{
 		name: 'authentication.signUp',
 		path: '/sign-up',
 		component: SignUp
+	},
+	{
+		name: 'authentication.logIn',
+		path: '/log-in',
+		component: LogIn
 	}
 ];
 
