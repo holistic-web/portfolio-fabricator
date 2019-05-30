b159d62360a3ca77efe6692db90046adfb17017e
Charlie Lancaster <charlie.lanc@gmail.com>
2019-05-30 23:41:40 +0100

    feat: added login container

---
+++ portfolio-management/admin-ui/src/containers/authentication/LogIn.vue
@@ -0,0 +1,76 @@
+<template>
+	<div class="LoginAccount">
+		<h1>Log in to your Account</h1>
+
+		<b-form @submit="login" class="LoginAccount__form">
+
+			<b-form-group label="Enter your Email Address">
+				<b-form-input
+					v-model.trim="user.email"
+					type="email"
+					required
+					placeholder="Email Address"/>
+			</b-form-group>
+
+			<b-form-group label="Enter your Password">
+				<b-form-input
+					v-model="newUser.password"
+					type="password"
+					required
+					placeholder="Password"/>
+			</b-form-group>
+
+			<b-button
+				type="submit"
+				variant="primary">
+				Log In
+			</b-button>
+
+			<b-button
+				type="link"
+				variant="secondary">
+				Create Account
+			</b-button>
+
+		</b-form>
+	</div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+	data() {
+		return {
+			user: { },
+			errorText: null
+		}
+	},
+
+	methods: {
+		...mapActions ({
+			loginAccount: 'account/logIn'
+		}),
+		async login() {
+			try {
+				await this.loginAccount({
+					username: this.user.email,
+					password: this.user.password
+				});
+				this.errorText = "success!";
+			} catch (err) {
+				this.errorText = err.message;
+			}
+
+		},
+	}
+};
+</script>
+
+<style lang="scss">
+
+.LoginAccount {
+	padding: 1rem;
+}
+
+</style>
