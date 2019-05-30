fb8df159bfce3e1b32fb3bc902ffdb3caf21c5ff
Charlie Lancaster <charlie.lanc@gmail.com>
2019-05-30 23:11:46 +0100

    feat: removed unused routes and updated store location

--- portfolio-management/admin-ui/src/main.js
+++ portfolio-management/admin-ui/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/index';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
--- portfolio-management/admin-ui/src/router.js
+++ portfolio-management/admin-ui/src/router.js
@@ -1,17 +1,4 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './containers/Home.vue';
 
 Vue.use(Router);
-
-export default new Router({
-	mode: 'history',
-	base: process.env.BASE_URL,
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			component: Home
-		}
-	]
-});
