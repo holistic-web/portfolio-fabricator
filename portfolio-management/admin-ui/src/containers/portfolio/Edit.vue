231e954d219d9e26d1e52b423ac247adcfee3fb6
michaelfitzhavey <michael.fitzhavey@gmail.com>
2019-05-31 02:29:02 +0100

    feat: tidy

--- portfolio-management/admin-ui/src/containers/portfolio/Edit.vue
+++ portfolio-management/admin-ui/src/containers/portfolio/Edit.vue
@@ -2,32 +2,53 @@
 	<section class="PortfolioEdit">
 		<h1>Edit</h1>
 
-  		<schema-form :schema="schema" v-model="formData" />
+		<schema-form v-if="portfolio" :schema="schema" v-model="formData"/>
+
+		<b-button v-text="'Update'" @click="onUpdateClick"/>
 	</section>
 </template>
 
 <script>
-import Vue from 'vue';
-import Buefy from 'buefy';
-import {SchemaForm} from 'vue-json-schema-form';
+import { SchemaForm } from 'vue-json-schema-form';
+import { mapActions, mapGetters } from 'vuex';
 import jsonSchema from '../../../../portfolio.schema.json';
 
-import 'buefy/dist/buefy.css';
-
-Vue.use(Buefy);
-
 export default {
-  name: "SchemaFormDemo",
-  components: {
-    SchemaForm
-  },
-  data() {
-    return {
-      schema: jsonSchema,
-      formData: {}
-    };
-  }
-}
+	components: {
+		SchemaForm
+	},
+	data() {
+		return {
+			schema: jsonSchema,
+			formData: {}
+		};
+	},
+	computed: {
+		...mapGetters({
+			portfolio: 'portfolio/portfolio'
+		})
+	},
+	methods: {
+		...mapActions({
+			fetchPortfolio: 'portfolio/fetchPortfolio',
+			updatePortfolio: 'portfolio/updatePortfolio'
+		}),
+		async onUpdateClick() {
+			await this.updatePortfolio(this.formData);
+		}
+	},
+	watch: {
+		portfolio: {
+			immediate: true,
+			handler() {
+				this.formData = this.portfolio;
+			}
+		}
+	},
+	created() {
+		this.fetchPortfolio();
+	}
+};
 </script>
 
 <style lang="scss">
--- portfolio-management/admin-ui/src/main.js
+++ portfolio-management/admin-ui/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
 import App from './App.vue';
 import router from './router/index';
 import store from './store/index';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.use(Buefy);
 
 new Vue({
 	router,
