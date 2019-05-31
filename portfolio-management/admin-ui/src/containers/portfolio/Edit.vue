aed3303ac0a4ae8c4bbbe1009309b2ad2f56ada9
Charlie Lancaster <charlie.lanc@gmail.com>
2019-05-31 02:05:50 +0100

    feat: added schema form to Edit page

---
+++ portfolio-management/admin-ui/src/containers/portfolio/Edit.vue
@@ -0,0 +1,37 @@
+<template>
+	<section class="PortfolioEdit">
+		<h1>Edit</h1>
+
+  		<schema-form :schema="schema" v-model="formData" />
+	</section>
+</template>
+
+<script>
+import Vue from 'vue';
+import Buefy from 'buefy';
+import {SchemaForm} from 'vue-json-schema-form';
+import jsonSchema from '../../../../portfolio.schema.json';
+
+import 'buefy/dist/buefy.css';
+
+Vue.use(Buefy);
+
+export default {
+  name: "SchemaFormDemo",
+  components: {
+    SchemaForm
+  },
+  data() {
+    return {
+      schema: jsonSchema,
+      formData: {}
+    };
+  }
+}
+</script>
+
+<style lang="scss">
+.PortfolioEdit {
+	padding: 1rem;
+}
+</style>
