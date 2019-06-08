2c5417f3016187a736ca96fe449a7d226e628618
michaelfitzhavey <michael.fitzhavey@gmail.com>
2019-06-08 20:37:22 +0100

    fix: cloud function

--- portfolio-management/cloud-functions/functions/index.js
+++ portfolio-management/cloud-functions/functions/index.js
@@ -1,19 +1,18 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+admin.initializeApp(functions.config().firebase);
+
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.createPortfolioOnNewAccount = functions.auth.user().onCreate(async (user) => {
 	const uid = user.uid;
 	const email = user.email;
-	console.log('uid: ', uid);
 	const portfolioCollection = admin.firestore().collection('portfolios');
-	console.log('portfolioCollection: ', portfolioCollection);
 	await portfolioCollection.doc(uid).set({
 		contact: {
-			email: useremail
+			email: email
 		}
 	});
-	console.log('done');
 });
