25973d6ff3cbd31cd63656582f482dca6c13be21
michaelfitzhavey <michael.fitzhavey@gmail.com>
2019-06-15 01:17:11 +0100

    chore: simplify modal

--- fabricated-website/src/components/ContactModal.vue
+++ fabricated-website/src/components/ContactModal.vue
@@ -1,42 +1,36 @@
 <template>
 	<b-modal
+		body-class="ContactModal"
 		:visible="visible"
 		title="ContactModal"
-		@hidden="$emit('hidden')"
-		ok-title="Send">
-		<p class="my-4">Send me a message by filling out the form below!</p>
-		<b-card bg-variant="light">
-			<b-form-group
-			label-cols-lg="3"
-			label="Your Details"
-			label-size="lg"
-			label-class="font-weight-bold pt-0"
-			class="mb-0">
-				<b-form-group
-					label-cols-sm="3"
-					label="Name"
-					label-align-sm="right"
-					label-for="nested-name">
-					<b-form-input id="nested-name"/>
-				</b-form-group>
+		ok-title="Send"
+		@hidden="$emit('hidden')">
 
-				<b-form-group
-					label-cols-sm="3"
-					label="Email"
-					label-align-sm="right"
-					label-for="nested-email">
-					<b-form-input id="nested-email"/>
-				</b-form-group>
+		<p v-text="'Send me a message by filling out the form below!'"/>
 
-				<b-form-group
-					label-cols-sm="3"
-					label="Message"
-					label-align-sm="right"
-					label-for="nested-message">
-					<b-form-input id="nested-message" size="lg"/>
-				</b-form-group>
-			</b-form-group>
-		</b-card>
+		<h3 v-text="'Your Details'"/>
+
+		<b-form-group
+			label="Name"
+			label-align-sm="right"
+			label-for="ContactModal__name">
+			<b-form-input id="ContactModal__name"/>
+		</b-form-group>
+
+		<b-form-group
+			label="Email"
+			label-align-sm="right"
+			label-for="ContactModal__email">
+			<b-form-input id="ContactModal__email"/>
+		</b-form-group>
+
+		<b-form-group
+			label="Message"
+			label-align-sm="right"
+			label-for="ContactModal__message">
+			<b-form-input id="ContactModal__message" size="lg"/>
+		</b-form-group>
+
 	</b-modal>
 </template>
 
@@ -50,3 +44,11 @@ export default {
 	}
 };
 </script>
+
+<style lang="scss">
+.ContactModal {
+	display: flex;
+	flex-direction: column;
+	text-align: left;
+}
+</style>
