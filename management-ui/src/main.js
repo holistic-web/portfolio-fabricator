385e067d4527812dc5fc9e9e3e32b5a6e8469ac4
Charlie Lancaster <charlie.lancaster@yousoft.com>
2020-03-17 12:15:39 +0000

    chore: remove old reference to old schema generator

--- management-ui/src/main.js
+++ management-ui/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import VueIcon from 'vue-icon';
 import VueToasted from 'vue-toasted';
-import VueJsonSchemaForm from 'vue-json-schema-form';
+import FormVueLatte from 'formvuelatte';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import App from './App.vue';
@@ -13,7 +13,7 @@ Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(VueIcon, 'v-icon');
 Vue.use(VueToasted);
-Vue.use(VueJsonSchemaForm);
+Vue.use(FormVueLatte);
 Vue.use(Buefy);
 
 new Vue({
