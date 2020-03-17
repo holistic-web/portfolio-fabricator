d62e203c1d88e98d8cf0dfd6a16dcca76f9fe3f5
Charlie Lancaster <charlie.lancaster@yousoft.com>
2020-03-17 12:11:31 +0000

    chore: add index file for easy exporting of components

---
+++ management-ui/src/components/form/index.js
@@ -0,0 +1,15 @@
+import FormInput from './FormInput.vue';
+import FormTextArea from './FormTextArea.vue';
+
+const components = [
+	{
+		name: 'form-input',
+		component: FormInput
+	},
+	{
+		name: 'form-text-area',
+		component: FormTextArea
+	}
+];
+
+export default components;
