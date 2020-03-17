31a9ad309a4994b4e1374433aef0dd925f090e85
Charlie Lancaster <charlie.lancaster@yousoft.com>
2020-03-17 12:10:51 +0000

    feat: add custom form components for formvuelatte

---
+++ management-ui/src/components/form/FormTextArea.vue
@@ -0,0 +1,23 @@
+<template>
+	<b-form-textarea :value="value"
+    @input="onInput($event.target.value)" />
+</template>
+
+<script>
+import { FormMixin } from 'formvuelatte';
+
+export default {
+	mixins: { FormMixin },
+	props: {
+		value: {
+			type: String,
+			required: true
+		}
+	},
+	methods: {
+		onInput(data) {
+			this.$emit('input', data);
+		}
+	}
+};
+</script>
